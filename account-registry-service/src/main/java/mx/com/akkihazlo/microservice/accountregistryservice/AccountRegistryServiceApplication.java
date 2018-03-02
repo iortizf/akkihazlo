491a263cd7b2060db73a18facf7b889f2967929e
Israel Ortiz Feria <iortiz.feria@gmail.com>
2018-03-02 17:01:14 -0600

    Registration service

--- account-registry-service/src/main/java/mx/com/akkihazlo/microservice/accountregistryservice/AccountRegistryServiceApplication.java
+++ account-registry-service/src/main/java/mx/com/akkihazlo/microservice/accountregistryservice/AccountRegistryServiceApplication.java
@@ -2,8 +2,10 @@ package mx.com.akkihazlo.microservice.accountregistryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AccountRegistryServiceApplication {
 
 	public static void main(String[] args) {
